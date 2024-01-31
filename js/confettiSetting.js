84dccec6b8b964b7947e96874874f9dc11897aba
Sebastian <74075156+sebastianSSN@users.noreply.github.com>
2024-01-30 23:11:32 -0500

    Add files via upload

---
+++ js/confettiSetting.js
@@ -0,0 +1,6 @@
+let confetti = new Confetti("tik");
+confetti.setCount(752);
+confetti.setSize(1);
+confetti.setPower(30);
+confetti.setFade(false);
+confetti.destroyTarget(false);
---
+++ js/index.js
@@ -0,0 +1,18 @@
+function openPopup() {
+  document.getElementById("popup").style.display = "flex";
+}
+
+function closePopup() {
+  document.getElementById("popup").style.display = "none";
+}
+
+function openPinterest() {
+  window.open("https://pin.it/6IgHUiqoH", "_blank");
+}
+
+function openPlaylist() {
+  window.open(
+    "https://open.spotify.com/playlist/4PkJ4gJXVZcxoGyGsbCqjy?si=5562bfe3ec8b478b",
+    "_blank"
+  );
+}
