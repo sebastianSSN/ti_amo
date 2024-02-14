9b2f6c5086c9e58af62e31a003b9dbc9b3f26a65
Sebastian <74075156+sebastianSSN@users.noreply.github.com>
2024-02-13 23:11:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ document.getElementById('siBtn').addEventListener('click', function() {
 
     // Mostrar el mensaje específico
     document.getElementById('messageContainer').style.display = 'block';
-    document.getElementById('messageContainer').innerHTML = '¡Oh Siii! jajaja';
+    document.getElementById('messageContainer').innerHTML = 'te amohermosa 💕';
 
     // Mostrar otro gif después de 3 segundos
     setTimeout(function() {
